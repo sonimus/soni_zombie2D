4007e88d400382cc0b166f99ae0571ce0b09e261
sonimus <soniaparrilla4a@gmail.com>
2014-11-10 18:53:47 +0100

    scribbala sin terminar

---
+++ Assets/Scripts/bala.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bala : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		rigidbody2D.velocity = velocity * transform.animation 
+	}
+	
