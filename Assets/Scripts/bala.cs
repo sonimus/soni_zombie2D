a82669536cc1cf6f6fad357a8ada6ede8296951a
sonimus <soniaparrilla4a@gmail.com>
2014-11-12 16:55:26 +0100

    creadas capas

--- Assets/Scripts/bala.cs
+++ Assets/Scripts/bala.cs
@@ -2,9 +2,9 @@
 using System.Collections;
 
 public class bala : MonoBehaviour {
-
+	public Vector2 velocity;
 	// Use this for initialization
 	void Start () {
-		rigidbody2D.velocity = velocity * transform.animation 
+		rigidbody2D.velocity = velocity; //* transform.localScale;
 	}
-	
+}
