d4383037c7badec34dd232cc03efdc1728f24728
sonimus <soniaparrilla4a@gmail.com>
2014-11-12 18:44:56 +0100

    sistema de particulas

--- Assets/Scripts/bala.cs
+++ Assets/Scripts/bala.cs
@@ -2,9 +2,23 @@
 using System.Collections;
 
 public class bala : MonoBehaviour {
-	public Vector2 velocity;
-	// Use this for initialization
+	public Vector2 velocity = new Vector2(5,0);
+	public GameObject particulas;
+	//public GameObject blood;
+	//public int damage = 5;
+
 	void Start () {
-		rigidbody2D.velocity = velocity; //* transform.localScale;
+		rigidbody2D.AddForce (velocity * transform.localScale.x, ForceMode2D.Impulse);
+
 	}
-}
+	void OnCollisionEnter2D(Collision2D target){
+				onDestroy();
+
+	}
+		void onDestroy(){
+			var clone = Instantiate(particulas,transform.position,Quaternion.identity) as GameObject;
+			Destroy (clone,1);
+			Destroy (gameObject);
+		}
+  }
+
