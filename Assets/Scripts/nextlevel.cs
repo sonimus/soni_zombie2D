ae079a4b77e90342da983c4e76a14ba4c94d6b9a
sonimus <soniaparrilla4a@gmail.com>
2014-12-03 18:56:06 +0100

    pasar de una escena a otra

---
+++ Assets/Scripts/nextlevel.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class nextlevel : MonoBehaviour {
+	
+	public string nombreNivel = "zombie3";
+		bool cargando = false;
+
+
+	//void Update(){
+		//if (Input.GetKeyDown (KeyCode.Space) && delantepuerta) {
+		//	teletransporta = true;
+		//}
+	//}
+	void OnTriggerEnter2D (Collider2D target){
+		if (target.transform.tag == "Player")
+						Application.LoadLevel (nombreNivel);
+	}
+	
+	//void OnTriggerStay2D(Collider2D target){
+	//	if (teletransporta){ 
+			//target.transform.position = destino.position;
+			//teletransporta = false;
+			//delantepuerta = false;
+		//}
+	//}
+	//void OnTriggerExit2D (Collider2D target){
+		//if (target.transform.tag == "Player")
+		//	delantepuerta = false;
+	//}
+	
+}
--- Assets/Scripts/teleport.cs
+++ Assets/Scripts/teleport.cs
@@ -4,18 +4,34 @@ using System.Collections;
 public class teleport : MonoBehaviour {
 	
 	public Transform destino;
+	public bool delantepuerta = false;
+	public bool teletransporta = false;
 	//public float tiempoEspera = 0.0f; // Tiempo que el Player tiene que estar para cargar
 	//bool cargando = false; // Controla si el player esta dentro del trigger
 	
+	void Update(){
+				if (Input.GetKeyDown (KeyCode.Space) && delantepuerta) {
+			teletransporta = true;
+				}
+		}
+	void OnTriggerEnter2D (Collider2D target){
+				if (target.transform.tag == "Player")
+						delantepuerta = true;
+		}
 	
-	
-	void OnTriggerStay2D(Collider2D target) {
-		if (target.transform.tag == "Player") 
+	void OnTriggerStay2D(Collider2D target){
+		if (teletransporta){ 
 			target.transform.position = destino.position;
+			teletransporta = false;
+			delantepuerta = false;
+	     }
+       }
+	void OnTriggerExit2D (Collider2D target){
+		if (target.transform.tag == "Player")
+			delantepuerta = false;
 	}
+	
 }
-	
-	
 	//void OnTriggerStay2D(Collider2D target){
 		
 		//if (target.transform.tag == "Player") {
