8f56eedd70af0fade20d58525d4158152d9722f8
sonimus <soniaparrilla4a@gmail.com>
2014-12-04 19:41:55 +0100

    problemas
    voy atrasada

---
+++ Assets/Scripts/activarcanvas.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class activarcanvas : MonoBehaviour {
+	public GameObject canvas;
+	public bool estacerca;
+	public bool muestratexto = false;
+	
+
+	void OnTriggerEnter2D (Collider2D col){
+				estacerca = true;
+		}
+	void OnTriggerEnter2D (Collider2D col){
+				estacerca = false;
+		}
+
+	void Update () {
+	if (estacerca){
+				canvas.SetActive (true);
+			}else{
+				canvas.SetActive(false);
+	}
+   }
+
+}
---
+++ Assets/Scripts/generadorZombies.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class generadorZombies : MonoBehaviour {
+	public GameObject Zombie;
+	public float spamTime = 5f;
+	public int maxZombies = 10;
+
+	private float nextZombie;
+	private float zcount = 0;
+
+	void Start () {
+		nextZombie = spamTime;
+	}
+	
+
+	void Update () {
+	if (Time.time > nextZombie) && (zcount < maxZombies)){
+			nextZombie = Time.time + spamTime;
+			var clone = Instantiate (Zombie, transform.position, Quaternion.identity) as GameObject;
+			zcount++;
+	       }
+        }
+
+}
